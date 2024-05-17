8b3d48ae45746783ee36106ac0ea229acc9d02be
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-17 10:05:33 +0200

    loacal storage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
-
 import ContactForm from "../contactForm/ContactForm";
 import SearchBox from "../searchBox/SearchBox";
 import ContactList from "../contactList/ContactList";
@@ -8,20 +7,20 @@ import testData from "../testData.json";
 import css from "./App.module.css";
 
 const App = () => {
-  const [searchFilter, setSearchFilter] = useState("");
-  const [userContacts, setUserContacts] = useState(() => {
-    const contacts = window.localStorage.getItem("contacts");
+  
+  // const [userContacts, setUserContacts] = useState(() => {
+  //   const contacts = window.localStorage.getItem("contacts");
 
-    if (contacts !== null) {
-      return JSON.parse(contacts);
-    }
+  //   if (contacts !== null) {
+  //     return JSON.parse(contacts);
+  //   }
 
-    return testData;
-  });
+  //   return testData;
+  // });
 
-  useEffect(() => {
-    window.localStorage.setItem("contacts", JSON.stringify(userContacts));
-  }, [userContacts]);
+  // useEffect(() => {
+  //   window.localStorage.setItem("contacts", JSON.stringify(userContacts));
+  // }, [userContacts]);
 
  
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import App from "./components/App/App";
 import "./index.css";
+import { persistor } from "./components/redux/store";
 import { Provider } from "react-redux";
 import { store } from "./components/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <Provider store={store}>
-     <App />
-   </Provider>
+    <Provider store={store}>
+      <PersistGate loading="wait... for it" persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
+

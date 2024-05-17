218857c682362b2f7d265205f4fcf054f7964509
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-05-17 10:07:39 +0200

    logs removed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,36 +1,14 @@
-import { useState, useEffect } from "react";
-import { nanoid } from "nanoid";
 import ContactForm from "../contactForm/ContactForm";
 import SearchBox from "../searchBox/SearchBox";
 import ContactList from "../contactList/ContactList";
-import testData from "../testData.json";
 import css from "./App.module.css";
 
 const App = () => {
-  
-  // const [userContacts, setUserContacts] = useState(() => {
-  //   const contacts = window.localStorage.getItem("contacts");
-
-  //   if (contacts !== null) {
-  //     return JSON.parse(contacts);
-  //   }
-
-  //   return testData;
-  // });
-
-  // useEffect(() => {
-  //   window.localStorage.setItem("contacts", JSON.stringify(userContacts));
-  // }, [userContacts]);
-
- 
-
-  
-
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
       <ContactForm />
-      <SearchBox  />
+      <SearchBox />
       <ContactList />
     </div>
   );
--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -28,7 +28,6 @@ export default function LoginForm() {
         initialValues={{
           name: "",
           phoneNumber: "",
-          // id: "",
         }}
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
